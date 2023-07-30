a43b5ef2367126c7a34623e8b88334142c90349b
“Matias <matiascorrea420@gmail.com>
2023-07-30 05:34:06 -0300

    Finish day 1

---
+++ day-1/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	caloriesList := "./big-input.txt"
+
+	buffer, err := os.Open(caloriesList)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fileScanner := bufio.NewScanner(buffer)
+	fileScanner.Split(bufio.ScanLines)
+
+	var calories []string
+
+	for fileScanner.Scan() {
+		calories = append(calories, fileScanner.Text())
+	}
+
+	buffer.Close()
+
+	var totalCalories []int
+	caloriesSum := int64(0)
+
+	for _, c := range calories {
+		if c == "" {
+			totalCalories = append(totalCalories, int(caloriesSum))
+			caloriesSum = 0
+		} else {
+			itemCalorie, _ := strconv.ParseInt(c, 10, 0)
+			caloriesSum = caloriesSum + itemCalorie
+		}
+	}
+
+	sort.Slice(totalCalories, func(a, b int) bool {
+		return totalCalories[a] > totalCalories[b]
+	})
+
+	fmt.Println(totalCalories[0], totalCalories[1], totalCalories[2])
+
+	fmt.Println(totalCalories[0] + totalCalories[1] + totalCalories[2])
+
+}
